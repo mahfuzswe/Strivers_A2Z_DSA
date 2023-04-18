52dc4a4c42a3c4747169a44a17e0ef5b91d55f15
Invincible-21 <shantobd1969@gmail.com>
2023-04-19 01:28:33 +0600

    initial commit

---
+++ LearnC++.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    // int, long, long long, float, double
+    // string and getline
+
+    // string s1, s2;
+    // cin >> s1 >> s2;
+    // cout << s1 << " " << s2;
+
+    // string str;
+    // getline(cin, str);
+
+    // cout << str << endl;
+
+    // char
+    //
+
+    // int day;
+    // cin >> day;
+    // switch (day)
+    // {
+    // case 1:
+    //     cout << "Saturday\n";
+    //     break;
+
+    // case 2:
+    //     cout << "Sunday\n";
+    //     break;
+    // case 3:
+    //     cout << "Monday\n";
+    //     break;
+    // case 4:
+    //     cout << "Tuesday\n";
+    //     break;
+    // }
+
+    // int a,b,c,d;
+    // cin>>a>>b>>c>>d;
+
+    int arr[5];
+    cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+
+    arr[3] += 10;
+    cout << arr[3];
+
+    return 0;
+}

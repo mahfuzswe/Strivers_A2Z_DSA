2aace5255049178c8edfcd2acf0124247457d4d8
Invincible-21 <shantobd1969@gmail.com>
2023-04-19 15:22:01 +0600

    updated

---
+++ Time_and_Space_Complexity.cpp
@@ -0,0 +1,24 @@
+/*
+    # What is Time Complexity? --> TC != Time taken
+    --> Rate at which the time taken increases with
+        respect to input size rather.
+
+    * TC --> worst case scenario
+    * avoid constants
+    * avoid lower values
+    *
+
+    # Space Complexity --> memory space
+    --> Auxiliary space --> space that you take to solve the problem
+             +
+        input space --> space that you take to store the input
+
+
+    *** NEVER DO ANYTHING TO INPUT
+
+    # CP --> Time Limit 1s --> 10^8 operations
+                        2s --> 2 x 10^8 operations
+                        5s --> 5 x 10^8 operations
+
+
+*/

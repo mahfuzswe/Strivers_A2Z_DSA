07c4bfc3d0459c50c3e9e9250ab6f258733a1102
Invincible-21 <shantobd1969@gmail.com>
2023-04-20 23:14:15 +0600

    patterns completed

--- Patterns/Patterns.cpp
+++ Patterns/Patterns.cpp
@@ -267,6 +267,189 @@ void pattern14(int n)
     }
 }
 
+void pattern15(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'A'; ch <= 'A' + (n - i - 1); ch++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern16(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        char ch = 'A' + i;
+        for (int j = 0; j <= i; j++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern17(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        // characters
+        char ch = 'A';
+        int breakpoint = (2 * i + 1) / 2;
+        for (int j = 1; j <= 2 * i + 1; j++)
+        {
+            cout << ch;
+            if (j <= breakpoint)
+                ch++;
+            else
+                ch--;
+        }
+        // space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern18(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'E' - i; ch <= 'E'; ch++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern19(int n)
+{
+    int iniS = 0; // initial Space
+    for (int i = 0; i < n; i++)
+    {
+        // stars
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "*";
+        }
+
+        // spaces
+        for (int j = 0; j <= iniS; j++)
+        {
+            cout << " ";
+        }
+
+        // stars
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "*";
+        }
+        iniS += 2;
+        cout << endl;
+    }
+
+    iniS = 8;
+    for (int i = 1; i <= n; i++)
+    {
+        // stars
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+
+        // spaces
+        for (int j = 0; j <= iniS; j++)
+        {
+            cout << " ";
+        }
+
+        // stars
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        iniS -= 2;
+        cout << endl;
+    }
+}
+
+void pattern20(int n)
+{
+    int spaces = 2 * n - 2;
+    for (int i = 1; i <= 2 * n - 1; i++)
+    {
+        int stars = i;
+        if (i > n)
+            stars = 2 * n - i;
+        // stars
+        for (int j = 1; j <= stars; j++)
+        {
+            cout << "*";
+        }
+        // spaces
+        for (int j = 0; j <= spaces; j++)
+        {
+            cout << " ";
+        }
+
+        // stars
+        for (int j = 1; j <= stars; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+
+        if (i < n)
+            spaces -= 2;
+        else
+            spaces += 2;
+    }
+}
+
+void pattern21(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0 || j == 0 || i == n - 1 || j == n - 1)
+            {
+                cout << "*";
+            }
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern22(int n)
+{
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+        for (int j = 0; j < 2 * n - 1; j++)
+        {
+            int top = i;
+            int left = j;
+            int right = (2 * n - 2) - j;
+            int down = (2 * n - 2) - i;
+            cout << (n - min(min(top, down), min(left, right)));
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     int n;
@@ -286,6 +469,14 @@ int main()
     // pattern12(n);
     // pattern13(n);
     // pattern14(n);
+    // pattern15(n);
+    // pattern16(n);
+    // pattern17(n);
+    // pattern18(n);
+    // pattern19(n);
+    // pattern20(n);
+    // pattern21(n);
+    pattern22(n);
 
     return 0;
 }
---
+++ Patterns/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'E' - i; ch <= 'E'; ch++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }

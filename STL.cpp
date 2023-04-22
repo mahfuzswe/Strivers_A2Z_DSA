accb26b1c614412c9d4a374c6c278b03c5100240
Invincible-21 <shantobd1969@gmail.com>
2023-04-23 01:44:49 +0600

    updated

--- STL.cpp
+++ STL.cpp
@@ -93,16 +93,218 @@ void explainVectors()
 
     // *** Insert Function ***
 
-    for (auto it : v)
-    {
-        cout << it << " ";
-    }
+    vector<int> v(2, 100);          // {100,100}
+    v.insert(v.begin(), 300);       // {300, 100, 100}
+    v.insert(v.begin() + 1, 2, 10); // {300, 10, 10, 100, 100}
+
+    vector<int> copy(2, 50);                       // {50,50}
+    v.insert(v.begin(), copy.begin(), copy.end()); // {50, 50, 300, 10, 10, 100, 100}
+
+    // {10,20}
+    cout << v.size(); // 2
+
+    //{10,20}
+    v.pop_back(); //{10}
+
+    // v1 --> {10,20}
+    // v2 --> {30,40}
+    v1.swap(v2); // v1 --> {30,40}, v2 --> {10,20}
+
+    v.clear(); // erases the entire vector
+
+    cout << v.empty(); // is it empty? --> True/False
+
+    // for (auto it : v)
+    // {
+    //     cout << it << " ";
+    // }
+}
+
+void explainList()
+{
+    list<int> ls;
+
+    ls.push_back(2);    //{2}
+    ls.emplace_back(4); // {2,4}
+
+    ls.push_front(5); // {5,2,4}
+    ls.emplace_front();
+
+    // rest functions same as vector
+    // begin, end, rbegin, rend, clear, insert, size, swap
+}
+
+void explainDeque()
+{
+    deque<int> dq;
+    dq.push_back(1);     //{1}
+    dq.emplace_back(2);  //{1,2}
+    dq.push_front(4);    //{4,1,2}
+    dq.emplace_front(3); //{3,4,1,2}
+
+    dq.pop_back();  // {3,4,1}
+    dq.pop_front(); // {4,1}
+
+    dq.back();
+
+    dq.front();
+
+    // rest functions same as vector
+    // begin, end, rbegin, rend , clear, insert, size, swap
+}
+
+//***Stack --> LIFO (Last in First out)
+
+void explainStack() // O(1) --> everythig happens in constant time
+{
+    stack<int> st;
+    st.push(1);    // {1}
+    st.push(2);    // {2,1}
+    st.push(3);    // {3,2,1}
+    st.push(3);    // {3,3,2,1}
+    st.emplace(5); // {5,3,3,2,1}
+
+    cout << st.top(); // it gives 5 but 5 is still on the top
+                      // when st.pop() it deletes
+
+    st.pop();           // st looks like {3,3,2,1}
+    cout << st.top();   // 3
+    cout << st.size();  // 4
+    cout << st.empty(); // false
+
+    stack<int> st1, st2;
+    st1.swap(st2);
+}
+
+void explainQuue()
+{ // similar to stack buu FIFO --> First in First Out
+  // O(1) --> all happens in constant time
+    queue<int> q;
+    q.push(1);    //{1}
+    q.push(2);    // {1,2}
+    q.emplace(4); // {1,2,4}
+
+    q.back() += 5;    // 9 --> here back will mean 4
+    cout << q.back(); // prints last element 9
+
+    // Q is {1,2,9}
+    cout << q.front(); // prints 1
+
+    q.pop(); // {2,9} --> deletes the front one
+
+    cout << q.front(); // prints 2
+
+    // size swap empty same as stack
+}
+
+/*
+    ** Priority Queue **
+    -> that has the largest value statys at the top
+    push --> O(log n)
+    top --> O(1)
+    pop --> O(log n)
+*/
+
+void explainPQ()
+{
+    // Maximum Heap
+    priority_queue<int> pq;
+
+    pq.push(5);     // {5}
+    pq.push(2);     // {5,2}
+    pq.push(8);     // {8,5,2}
+    pq.emplace(10); // {10,8,5,2}
+
+    cout << pq.top(); // prints 10
+
+    pq.pop(); // {8,5,2} --> 10 poped (removed)
+
+    cout << pq.top(); // prints 8
+
+    // size, swap, empty function same as others
+
+    // Minimum Heap
+    priority_queue<int, vector<int>, greater<int>> pq; // -> for minimum element at the top
+
+    pq.push(5);     // {5}
+    pq.push(2);     // {2,5}
+    pq.push(8);     // {2,5,8}
+    pq.emplace(10); // {2,5,8,10}
+
+    cout << pq.top(); // prints 2
+}
+
+/*
+    ** Set Container **
+    -> It stores everything in the sorted order
+    -> Stores unique // *UNIQUE
+
+*/
+
+void explainSet() // O(lon n)
+{
+    set<int> st;
+
+    st.insert(1);  // {1}
+    st.emplace(2); // {1,2}
+    st.insert(2);  // {1,2}
+    st.insert(4);  // {1,2,4}
+    st.insert(3);  // {1,2,3,4}
+
+    // Functionality of insert in vector
+    // can be used also, that only increases
+    // efficiency
+
+    // begin(), end(), rbegin(), rend(), size()
+    // empty() and swap() are same as those of above
+
+    // {1, 2, 3, 4, 5}
+    auto it = st.find(3); // treturs to the iterator which point to the 3
+
+    // {1, 2, 3, 4, 5}
+    auto it = st.find(6); // if an element is not here in the set ,
+    // it will always return st.end(); --> an iterator which point to right after the end
+
+    // {1,4,5}
+    st.erase(5); // erases 5 // takes logarithmic time
+
+    int cnt = st.count(1); // if 1 occerence it will gives 1 nor 0
+
+    auto it = st.find(3);
+    st.erase(it); // it takes constant time
+
+    // {1,2,3,4,5}
+    auto it1 = st.find(2);
+    auto it2 = st.find(4);
+    st.erase(it1, it2); // after erase {1,4,5} [first, last)
+
+    // lower_bound() and upper_bound() function works in the same way
+    // as in vector it does
+
+    // This is the syntax
+    auto it = st.lower_bound(2);
+
+    auto it = st.upper_bound(3);
+}
+
+void explainMultiSet()
+{
+    // everything is same as set
+    // only stores duplicate elements also
+
+    multiset<int> ms;
+
+    ms.insert(1); // {1}
+    ms.insert(1); // {1, 1}
+    ms.insert(1); // {1, 1, 1}
+
+    ms.erase(1); // all 1's erased
 }
 
 int main()
 {
     // explainPairs();
-    explainVectors();
+    // explainVectors();
 
     return 0;
 }

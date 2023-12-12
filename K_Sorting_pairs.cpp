5667047fa2776661b7f35ad40a648c2b4ab2c9dd
Invincible-21 <shantobd1969@gmail.com>
2023-12-12 19:03:52 +0600

    let's march ahead

---
+++ K_Sorting_pairs.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define int long long
+#define double long double
+#define pb(e) push_back(e);
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define mp make_pair
+#define flp(i,a,b) for(int i=a; i<=b; i++)
+#define sq(a) (a)*(a)
+#define all(x) x.begin(),x.end()
+using namespace std;
+        
+void mr_phoenix(){
+   int t;
+   cin>>t;
+   while(t--){
+     string s; cin>>s;
+     int n; cin>>n;
+
+     
+  }
+}
+        
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    mr_phoenix();
+    
+    return 0;
+}

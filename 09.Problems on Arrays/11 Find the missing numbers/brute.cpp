8d9dc938840c97e8424c62c70adc6e9beb5a0c57
Mahfuzur Rahman Shanto <shantobd1969@gmail.com>
2025-03-10 00:14:18 +0600

    updated

---
+++ 09.Problems on Arrays/11 Find the missing numbers/brute.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int missingNumebr(int n, vector<int>&v){
+    for(int i = 1; i <= n; ++i){
+        int flg = 0;
+        for(int j = 0; j < n-1; j++){
+            if(v[j] == i){
+                flg = 1;
+                break;
+            }
+        }
+        if(flg == 0) return i;
+    }
+    return -1;
+}
+
+int main(){
+    int n; cin >> n;
+    vector<int>v(n-1);
+    for(int i = 0; i < n-1; ++i) cin >> v[i];
+
+    int ans = missingNumebr(n, v);
+    cout << "missing numebr: " << ans << '\n';
+
+    return 0;
+}
